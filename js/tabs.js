897c492eaab1347ede9e0610837f63eb1a06ee0d
MARCO LOPEZ <mlopez@virtualsip.net>
2012-10-24 23:16:15 -0500

    panel-izq

--- js/tabs.js
+++ js/tabs.js
@@ -1,45 +1,21 @@
-/* ------------------------------------------------------------------------
-	Do it when you're ready dawg!
-------------------------------------------------------------------------- */
+$(function(){
+	tabs.init();
+});
 
-	$(function(){
-		tabs.init();
-	});
-	$(function(){
-		tabs2.init();
-	});
-	
-	tabs = {
-		init : function(){
-			$('.tabs').each(function(){
-				$(this).find('.tab-content:gt(0)').hide();
+tabs = {
+	init : function(){
+		$('#nwi-pest').each(function(){
+			$(this).find('.pest-content:gt(0)').hide();
 
-				$(this).find('ul.nav a').click(function(){
-					$(this).parents('div.tabs').find('.tab-content').hide();
-					$($(this).attr('href')).show();
+			$(this).find('ul.nav a').click(function(){
+				$(this).parents('#nwi-pest').find('.pest-content').hide();
+				$($(this).attr('href')).show();
 
-					$(this).parent().addClass('selected').siblings().removeClass('selected');
+				$(this).parent().addClass('selected').siblings().removeClass('selected');
 
-					return false;
-				});
+				return false;
 			});
-			
-		}
+		});
+		
 	}
-	tabs2 = {
-		init : function(){
-			$('.tabs2').each(function(){
-				$(this).find('.tab-content2:gt(0)').hide();
-
-				$(this).find('ul.nav2 a').click(function(){
-					$(this).parents('div.tabs2').find('.tab-content2').hide();
-					$($(this).attr('href')).show();
-
-					$(this).parent().addClass('selected').siblings().removeClass('selected');
-
-					return false;
-				});
-			});
-			
-		}
-	}
+}
