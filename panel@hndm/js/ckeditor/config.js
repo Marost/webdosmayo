9a0f37383e1e1f30ff605c0cf56f2f5eeddd6b7e
MARCO LOPEZ <mlopez@virtualsip.net>
2012-11-11 23:39:40 -0500

    cambio de carpeta kcfinder

--- panel@hndm/js/ckeditor/config.js
+++ panel@hndm/js/ckeditor/config.js
@@ -4,11 +4,10 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
 CKEDITOR.editorConfig = function(config) {
-	var carpeta="panel@hndm";
-   config.filebrowserBrowseUrl = '/'.carpeta.'/js/kcfinder/browse.php?type=files';
-   config.filebrowserImageBrowseUrl = '/'.carpeta.'/js/kcfinder/browse.php?type=images';
-   config.filebrowserFlashBrowseUrl = '/'.carpeta.'/js/kcfinder/browse.php?type=flash';
-   config.filebrowserUploadUrl = '/'.carpeta.'/js/kcfinder/upload.php?type=files';
-   config.filebrowserImageUploadUrl = '/'.carpeta.'/js/kcfinder/upload.php?type=images';
-   config.filebrowserFlashUploadUrl = '/'.carpeta.'/js/kcfinder/upload.php?type=flash';
+   config.filebrowserBrowseUrl = '/panel@hndm/js/kcfinder/browse.php?type=files';
+   config.filebrowserImageBrowseUrl = '/panel@hndm/js/kcfinder/browse.php?type=images';
+   config.filebrowserFlashBrowseUrl = '/panel@hndm/js/kcfinder/browse.php?type=flash';
+   config.filebrowserUploadUrl = '/panel@hndm/js/kcfinder/upload.php?type=files';
+   config.filebrowserImageUploadUrl = '/panel@hndm/js/kcfinder/upload.php?type=images';
+   config.filebrowserFlashUploadUrl = '/panel@hndm/js/kcfinder/upload.php?type=flash';
 };
