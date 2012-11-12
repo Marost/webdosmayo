60370b3d4bbd7612755876b45232803c5e910d5b
MARCO LOPEZ <mlopez@virtualsip.net>
2012-11-12 14:48:53 -0500

    sin select

--- panel@hndm/paginas/noticias/especialidades/form-agregar.php
+++ panel@hndm/paginas/noticias/especialidades/form-agregar.php
@@ -28,9 +28,7 @@ $hora_actual=$hora[0].":".$hora[1];
 <script type="text/javascript" src="../../../js/ckeditor/ckeditor.js"></script>
 
 <!-- SPRY -->
-<script src="/SpryAssets/SpryValidationSelect.js" type="text/javascript"></script>
 <script src="/SpryAssets/SpryTabbedPanels.js" type="text/javascript"></script>
-<link href="/SpryAssets/SpryValidationSelect.css" rel="stylesheet" type="text/css" />
 <link href="/SpryAssets/SpryTabbedPanels.css" rel="stylesheet" type="text/css" />
 <link href="/SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
 <script src="/SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
