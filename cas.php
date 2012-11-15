4dc73573b8755744a8c5bb988598d2bce1d3e317
MARCO LOPEZ <mlopez@virtualsip.net>
2012-11-15 04:53:26 -0500

    ancho de cas

--- cas.php
+++ cas.php
@@ -53,7 +53,7 @@ require_once("panel@hndm/conexion/funcion-paginacion.php");
 
                                 <article>
 
-                                    <a href="#"><img src="imagenes/cas.png"></a>
+                                    <a class="imagen" href="#"><img src="imagenes/cas.png"></a>
 
                                     <div>
                                         <p><strong>PROCESO DE CONTRATACIÓN Nº 003-2012-HNDM-CECAS</strong></p>
