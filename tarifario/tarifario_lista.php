349bffb6a80f8156f718b326602985a1c81780fe
MARCO LOPEZ <mlopez@virtualsip.net>
2012-11-17 16:25:55 -0500

    cambio de modelo de tabla de tarifario

--- tarifario/tarifario_lista.php
+++ tarifario/tarifario_lista.php
@@ -13,14 +13,12 @@ if($tipo=="medicamento"){
 ?>
 <?php
 // Gets the data
-$id=isset($_POST['id']) ? $_POST['id'] : '';
-$search=isset($_POST['search']) ? $_POST['search'] : '';
-$multiple_search=isset($_POST['multiple_search']) ? $_POST['multiple_search'] : array();
-$items_per_page=isset($_POST['items_per_page']) ? $_POST['items_per_page'] : '';
-$sort=isset($_POST['sort']) ? $_POST['sort'] : '';
-$page=isset($_POST['page']) ? $_POST['page'] : 1;
-$total_items=(isset($_POST['total_items']) and $_POST['total_items']>=0) ? $_POST['total_items'] : '';
-$extra_cols=isset($_POST['extra_cols']) ? $_POST['extra_cols'] : array();
+$search=isset($_GET['ct_search']) ? $_GET['ct_search'] : '';
+$multiple_search=isset($_GET['ct_multiple_search']) ? $_GET['ct_multiple_search'] : array();
+$items_per_page=isset($_GET['ct_items_per_page']) ? $_GET['ct_items_per_page'] : '';
+$sort=isset($_GET['ct_sort']) ? $_GET['ct_sort'] : '';
+$page=isset($_GET['ct_page']) ? $_GET['ct_page'] : 1;
+$total_items=(isset($_GET['ct_total_items']) and $_GET['ct_total_items']>=0) ? $_GET['ct_total_items'] : '';
 
 // Uses the creativeTable to build the table
 include_once('../libs/creative_table/creativeTable.php');
@@ -28,48 +26,47 @@ include_once('../libs/creative_table/creativeTable.php');
 $ct=new CreativeTable();
 
 // Data Gathering
-$params['sql_query']                = "SELECT titulo,precio FROM DM_tarifario WHERE tipo=1";
-$params['search']                   = $search;
-$params['multiple_search']          = $multiple_search;
-$params['items_per_page']           = $items_per_page;
-$params['sort']                     = $sort;
-$params['page']                     = $page;
-$params['total_items']              = $total_items;
+$params['sql_query']            = "SELECT titulo, precio FROM DM_tarifario WHERE tipo=1"; 
+// IMPORTANT: you must specify the fields and not use * - it accept sql query in the form of SELECT ... FROM ... WHERE ... ORDER BY ... LIMIT
+$params['search']               = $search;
+$params['multiple_search']      = $multiple_search;
+$params['items_per_page']       = $items_per_page;
+$params['sort']                 = $sort;
+$params['page']                 = $page;
+$params['total_items']          = $total_items;
 
-// Layout Configurations
-$params['header']                   = 'Descripción,Precio';    // If you need to use the comma use &#44; instead of ,
-$params['width']                    = '600,100';
-
-// ***********************************************************************************
-// UNCOMMENT TO TEST THE DIFFERENTS OPTIONS AND SEE THE RESULTS AND TEST SOME YOURSELF
-
-// default: '$i*10';
-$params['items_per_page_init']    	= false;            // wont show the select box with the items per page
-$params['items_per_page_init']      = '10,20,50,100';   // creates the select box with those options
-$params['items_per_page_init']    	= '$i*10';          // you may configure with a function where $i is the iteration, in this case will be: 1*50,2*50,3*50,... resulting in 50,100,150,... dont forget to include the slashes in the string
-
-// default: '#TOTAL_ITEMS#'
-$params['items_per_page_all']       = false;            // wont show the show all option in the select box
-$params['items_per_page_all']     	= 'Show all';       // show the show all option with the text 'Show All'
-$params['items_per_page_all']     	= '#TOTAL_ITEMS#';  // show the show all option replacing the tag #TOTAL_ITEMS# with total of items value - default
-
-// default: ctItemsPerPageSubmitForm(\'#ID#\')
-//$params['items_per_page_url']     = 'alert(\'#ID# - #PAGE# - #ITEMS_PER_PAGE# - #TOTAL_ITEMS#\');';   // javascript code triggered on onchange event of the select box (you may use tags)
-
-
-// ***********************************************************************************
+// Layout Configurations (All - the commented lines are the default values)
+//$params['id']                 = 'ct';
+//$params['class']              = '';
+//$params['form_init']          = true;
+//$params['form_url']           = '';
+$params['header']               = 'Descripcion,Precio'; // If you need to use the comma use &#44; instead of , default: false
+$params['width']                = '185,,95,50'; // default: ''
+//$params['search_init']        = true;
+$params['search_html']          = '<span id="#ID#_search_value">Buscar...</span><a id="#ID#_advanced_search" href="javascript: ctShowAdvancedSearch(\'#ID#\');" title="Advanced Search"><img src="images/advanced_search.png" /></a><a href="javascript: ctSubmitForm(\'#ID#\',\'\',1);" style="margin-left: 10px; color: #555555; text-decoration: none;">Search</a>';
+$params['multiple_search_init'] = filled_array($multiple_search) ? true: 'hide';
+$params['items_per_page_init']  = '30*$i'; // default: '10*$i';
+//$params['items_per_page_all'] = '#TOTAL_ITEMS#';
+//$params['items_per_page_url'] = 'ctItemsPerPage(\'#ID#\')';
+//$params['sort_init']          = true;
+//$params['sort_order']         = 'adt';
+//$params['sort_url']           = 'ctSort(\'#ID#\',\'#COLUMN_ID#\')';
+//$params['extra_cols']         = array();
+//$params['odd_even']           = true;
+//$params['no_results']         = 'No results found.';
+//$params['actions']            = array();
+//$params['actions_url']        = 'ctActions(\'#ID#\')';
+//$params['pager']              = '';
+//$params['pager_ids']          = '';
+//$params['ajax_url']           = $_SERVER['PHP_SELF'];
+//$params['display_cols']       = '';
 
 $ct->table($params);
+
+// Insert a Pager into the table (I used this CreativePager Lite version because its very easy to use, but you may use any pager system that you like)
 $ct->pager = getCreativePagerLite('ct',$page,$ct->total_items,$ct->items_per_page);
 
-// If its an ajax call
-if($_POST['ajax_option']!=''){
-    echo json_encode($ct->display($_POST['ajax_option'],true));
-    exit;
-}else{
-    $out=$ct->display();
-}
-
+$out=$ct->display();
 ?>
 <link rel="stylesheet" type="text/css" href="/libs/creative_table/css/creative.css">
 <script src="/libs/creative_table/js/jquery-1.4.2.min.js"></script>
@@ -80,14 +77,12 @@ if($_POST['ajax_option']!=''){
 <?php }elseif($tipo=="procedimiento"){ ?>
 <?php
 // Gets the data
-$id=isset($_POST['id']) ? $_POST['id'] : '';
-$search=isset($_POST['search']) ? $_POST['search'] : '';
-$multiple_search=isset($_POST['multiple_search']) ? $_POST['multiple_search'] : array();
-$items_per_page=isset($_POST['items_per_page']) ? $_POST['items_per_page'] : '';
-$sort=isset($_POST['sort']) ? $_POST['sort'] : '';
-$page=isset($_POST['page']) ? $_POST['page'] : 1;
-$total_items=(isset($_POST['total_items']) and $_POST['total_items']>=0) ? $_POST['total_items'] : '';
-$extra_cols=isset($_POST['extra_cols']) ? $_POST['extra_cols'] : array();
+$search=isset($_GET['ct_search']) ? $_GET['ct_search'] : '';
+$multiple_search=isset($_GET['ct_multiple_search']) ? $_GET['ct_multiple_search'] : array();
+$items_per_page=isset($_GET['ct_items_per_page']) ? $_GET['ct_items_per_page'] : '';
+$sort=isset($_GET['ct_sort']) ? $_GET['ct_sort'] : '';
+$page=isset($_GET['ct_page']) ? $_GET['ct_page'] : 1;
+$total_items=(isset($_GET['ct_total_items']) and $_GET['ct_total_items']>=0) ? $_GET['ct_total_items'] : '';
 
 // Uses the creativeTable to build the table
 include_once('../libs/creative_table/creativeTable.php');
@@ -95,44 +90,47 @@ include_once('../libs/creative_table/creativeTable.php');
 $ct=new CreativeTable();
 
 // Data Gathering
-$params['sql_query']                = "SELECT titulo,precio FROM DM_tarifario WHERE tipo=2";
-$params['search']                   = $search;
-$params['multiple_search']          = $multiple_search;
-$params['items_per_page']           = $items_per_page;
-$params['sort']                     = $sort;
-$params['page']                     = $page;
-$params['total_items']              = $total_items;
+$params['sql_query']            = "SELECT titulo, precio FROM DM_tarifario WHERE tipo=2"; 
+// IMPORTANT: you must specify the fields and not use * - it accept sql query in the form of SELECT ... FROM ... WHERE ... ORDER BY ... LIMIT
+$params['search']               = $search;
+$params['multiple_search']      = $multiple_search;
+$params['items_per_page']       = $items_per_page;
+$params['sort']                 = $sort;
+$params['page']                 = $page;
+$params['total_items']          = $total_items;
 
-// Layout Configurations
-$params['header']                   = 'Descripción,Precio';    // If you need to use the comma use &#44; instead of ,
-$params['width']                    = '600,100';
-
-// ***********************************************************************************
-// UNCOMMENT TO TEST THE DIFFERENTS OPTIONS AND SEE THE RESULTS AND TEST SOME YOURSELF
-
-// default: '$i*10';
-$params['items_per_page_init']    	= false;            // wont show the select box with the items per page
-$params['items_per_page_init']      = '10,20,50,100';   // creates the select box with those options
-$params['items_per_page_init']    	= '$i*10';          // you may configure with a function where $i is the iteration, in this case will be: 1*50,2*50,3*50,... resulting in 50,100,150,... dont forget to include the slashes in the string
-
-// default: '#TOTAL_ITEMS#'
-$params['items_per_page_all']       = false;            // wont show the show all option in the select box
-$params['items_per_page_all']     	= 'Show all';       // show the show all option with the text 'Show All'
-$params['items_per_page_all']     	= '#TOTAL_ITEMS#';  // show the show all option replacing the tag #TOTAL_ITEMS# with total of items value - default
-
-// ***********************************************************************************
+// Layout Configurations (All - the commented lines are the default values)
+//$params['id']                 = 'ct';
+//$params['class']              = '';
+//$params['form_init']          = true;
+//$params['form_url']           = '';
+$params['header']               = 'Descripcion,Precio'; // If you need to use the comma use &#44; instead of , default: false
+$params['width']                = '185,,95,50'; // default: ''
+//$params['search_init']        = true;
+$params['search_html']          = '<span id="#ID#_search_value">Buscar...</span><a id="#ID#_advanced_search" href="javascript: ctShowAdvancedSearch(\'#ID#\');" title="Advanced Search"><img src="images/advanced_search.png" /></a><a href="javascript: ctSubmitForm(\'#ID#\',\'\',1);" style="margin-left: 10px; color: #555555; text-decoration: none;">Search</a>';
+$params['multiple_search_init'] = filled_array($multiple_search) ? true: 'hide';
+$params['items_per_page_init']  = '30*$i'; // default: '10*$i';
+//$params['items_per_page_all'] = '#TOTAL_ITEMS#';
+//$params['items_per_page_url'] = 'ctItemsPerPage(\'#ID#\')';
+//$params['sort_init']          = true;
+//$params['sort_order']         = 'adt';
+//$params['sort_url']           = 'ctSort(\'#ID#\',\'#COLUMN_ID#\')';
+//$params['extra_cols']         = array();
+//$params['odd_even']           = true;
+//$params['no_results']         = 'No results found.';
+//$params['actions']            = array();
+//$params['actions_url']        = 'ctActions(\'#ID#\')';
+//$params['pager']              = '';
+//$params['pager_ids']          = '';
+//$params['ajax_url']           = $_SERVER['PHP_SELF'];
+//$params['display_cols']       = '';
 
 $ct->table($params);
+
+// Insert a Pager into the table (I used this CreativePager Lite version because its very easy to use, but you may use any pager system that you like)
 $ct->pager = getCreativePagerLite('ct',$page,$ct->total_items,$ct->items_per_page);
 
-// If its an ajax call
-if($_POST['ajax_option']!=''){
-    echo json_encode($ct->display($_POST['ajax_option'],true));
-    exit;
-}else{
-    $out=$ct->display();
-}
-
+$out=$ct->display();
 ?>
 <link rel="stylesheet" type="text/css" href="/libs/creative_table/css/creative.css">
 <script src="/libs/creative_table/js/jquery-1.4.2.min.js"></script>
