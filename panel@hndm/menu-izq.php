d769db8a46555a16a21ce6d2e2ff672e76970079
MARCO LOPEZ <mlopez@virtualsip.net>
2012-11-16 23:02:14 -0500

    opcion de cas en el menu de administrador

--- panel@hndm/menu-izq.php
+++ panel@hndm/menu-izq.php
@@ -42,6 +42,9 @@ jmnpr(document).ready(function(){
         <a href="<?php echo $fila_empresa["web"]."".$carpeta_admin; ?>/paginas/noticias/capacitacion/listar.php">
           Capacitación</a></li>
       <li>
+        <a href="<?php echo $fila_empresa["web"]."".$carpeta_admin; ?>/paginas/noticias/capacitacion/listar.php">
+          CAS</a></li>
+      <li>
         <a href="<?php echo $fila_empresa["web"]."".$carpeta_admin; ?>/paginas/noticias/equipos-tecnologia/listar.php">
           Equipos de Última Tecnología</a></li>
       <li>
