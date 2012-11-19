1f7d7cc479b4291093f79ee1c5013766ba7cb880
MARCO LOPEZ <mlopez@virtualsip.net>
2012-11-19 10:54:23 -0500

    eliminacion de opcion de privilegios de usuario

--- panel@hndm/paginas/usuarios/administracion/actualizar.php
+++ panel@hndm/paginas/usuarios/administracion/actualizar.php
@@ -31,38 +31,6 @@ nombre='$nombre',
 apellidos='$apellidos', 
 email='$email' WHERE usuario='$usuario';",$conexion);
 
-mysql_query("UPDATE ".$tabla_suf."_usuario_privilegios SET 
-noticias_categoria=$categoria, 
-noticias=$noticia,
-noticias_multimedia=$multimedia, 
-noticias_reportero16=$reportero16, 
-noticias_not_superior=$noticia_superior, 
-noticias_comentarios=$noticia_comentarios, 
-noticias_modificar=$noticia_modificar, 
-noticias_eliminar=$noticia_eliminar, 
-portada=$portada, 
-portada_comentarios=$portada_comentarios, 
-portada_modificar=$portada_modificar, 
-portada_eliminar=$portada_eliminar, 
-columnistas=$columnistas, 
-columnistas_columna=$columna, 
-columnistas_comentarios=$columnistas_comentarios, 
-columnistas_modificar=$columnistas_modificar, 
-columnistas_eliminar=$columnistas_eliminar, 
-usuarios=$usuarios, 
-usuarios_modificar=$usuarios_modificar, 
-usuarios_eliminar=$usuarios_eliminar,
-encuesta=$encuesta, 
-encuesta_modificar=$encuesta_modificar, 
-encuesta_eliminar=$encuesta_eliminar,
-entrevista=$entrevista,
-entrevista_comentarios=$entrevista_comentarios,
-entrevista_modificar=$entrevista_modificar,
-entrevista_eliminar=$entrevista_eliminar,
-galeria=$galeria,
-galeria_modificar=$galeria_modificar,
-galeria_eliminar=$galeria_eliminar WHERE usuario='$usuario';", $conexion);
-
 if (mysql_errno()!=0)
 {
 	echo "error al insertar los datos ". mysql_errno() . " - ". mysql_error();
