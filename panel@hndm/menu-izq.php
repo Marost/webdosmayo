e5f8bbeff5d2ccca1db5e71b2737c846eda07280
MARCO LOPEZ <mlopez@virtualsip.net>
2012-11-19 08:08:06 -0500

    opcion libro de reclamaciones en administrador

--- panel@hndm/menu-izq.php
+++ panel@hndm/menu-izq.php
@@ -57,6 +57,9 @@ jmnpr(document).ready(function(){
         <a href="<?php echo $fila_empresa["web"]."".$carpeta_admin; ?>/paginas/noticias/historia/listar.php">
           Historia</a></li>
       <li>
+        <a href="<?php echo $fila_empresa["web"]."".$carpeta_admin; ?>/paginas/noticias/libro-reclamaciones/listar.php">
+          Libro de Reclamaciones</a></li>
+      <li>
         <a href="<?php echo $fila_empresa["web"]."".$carpeta_admin; ?>/paginas/noticias/noticias/listar.php">
           Noticias</a></li>
       <li>
