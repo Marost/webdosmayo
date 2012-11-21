b4ca8c132ec8777b013c207c4449cf5b2a4daa24
MARCO LOPEZ <mlopez@virtualsip.net>
2012-11-20 23:43:33 -0500

    index sin reflection

--- index.php
+++ index.php
@@ -80,13 +80,12 @@ $alianzas_url_final=$alianzas_categoria["url"]."/".$alianzas_id."-".$alianzas_ur
 
         <!-- SLIDER CAMPAÑAS -->
         <link href="/libs/allinone_banner/allinone_thumbnailsBanner.css" rel="stylesheet" type="text/css">
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <script src="http://code.jquery.com/jquery-latest.min.js"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
         <script src="/libs/allinone_banner/jquery.ui.touch-punch.min.js"></script>
         <script src="/libs/allinone_banner/jquery.mousewheel.min.js"></script>
         <script src="/libs/allinone_banner/allinone_thumbnailsBanner.js"></script>
-        <script src="/libs/allinone_banner/reflection.js" ></script>
-        <!--[if IE]><script src="/libs/allinone_banner/excanvas.compiled.js" ></script><![endif]-->
+        <!--[if IE]><script src="/libs/allinone_banner/excanvas.compiled.js" type="text/javascript"></script><![endif]-->
         <script>
         var jCampEsp=jQuery.noConflict();
         jCampEsp(document).ready(function(){
