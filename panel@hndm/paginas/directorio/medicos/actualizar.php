ff5d51a9717bbb9919670b35a9bdfb6d9d2f5db8
MARCO LOPEZ <mlopez@virtualsip.net>
2012-11-21 02:47:54 -0500

    cambio de nombre de carpeta de imagen en directorio administrador

--- panel@hndm/paginas/directorio/medicos/actualizar.php
+++ panel@hndm/paginas/directorio/medicos/actualizar.php
@@ -15,14 +15,10 @@ $fecha_publicacion=$_POST["fecha"];
 $hora_publicacion=$_POST["hora"];
 $fecha_pub=$fecha_publicacion." ".$hora_publicacion.":00";
 
-//SELECCIONAR REGISTRO PARA VERIFICAR IMAGEN
-$rst_query=mysql_query("SELECT * FROM ".$tabla_suf."_directorio WHERE id=$idnoticia LIMIT 1;", $conexion);
-$fila_query=mysql_fetch_array($rst_query);
 
 if($_POST['flash_uploader_0_tmpname']==""){
 	$imagen=$_POST["imagen_actual"];
 	$carpeta_imagen=$_POST["carpeta_imagen"];
-	$mostrar_imagen=1;
 }elseif($_POST['flash_uploader_0_tmpname']<>""){
 	$imagen=$_POST['flash_uploader_0_tmpname'];
 	$carpeta_imagen=fechaCarpeta()."/";
@@ -34,13 +30,13 @@ cargo='$cargo',
 contenido='$contenido', 
 imagen='$imagen', 
 dato_usuario='$usuario_user', 
-carpeta_imagen='$carpeta_imagen',
+imagen_carpeta='$carpeta_imagen',
 fecha_publicacion='$fecha_pub' WHERE id=$idnoticia;", $conexion);
 	
 if (mysql_errno()!=0){
-	//echo "error al insertar los datos ". mysql_errno() . " - ". mysql_error();
+	echo "error al insertar los datos ". mysql_errno() . " - ". mysql_error();
 	mysql_close($conexion);
-	header("Location:listar.php?mensaje=5");
+	//header("Location:listar.php?mensaje=5");
 } else {
 	//echo "error al insertar los datos ". mysql_errno() . " - ". mysql_error();
 	mysql_close($conexion);
