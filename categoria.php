8055b6656de38956ded92e705593cfe8f72cd96e
MARCO LOPEZ <mlopez@virtualsip.net>
2012-11-21 13:29:00 -0500

    paginacion en categoria de noticias

--- categoria.php
+++ categoria.php
@@ -19,6 +19,19 @@ $categoria_titulo=$fila_categoria["categoria"];
 /*LISTA DE NOTICIAS*/
 $rst_noticias=mysql_query("SELECT * FROM DM_noticia WHERE categoria=$categoria_id ORDER BY titulo ASC;", $conexion);
 
+################################################################
+//PAGINACION DE NOTICIAS
+require("class_pagination.php");
+
+//INICIO DE PAGINACION
+$page = (isset($_GET['page'])) ? intval($_GET['page']) : 1;
+$query     = mysql_query("SELECT COUNT(*) as count FROM DM_noticia WHERE categoria=$categoria_id;", $connect);
+$row       = mysql_fetch_assoc($query);
+$generated = intval($row['count']);
+$pagination = new Pagination("10", $generated, $page, $url_web."&page", 1, 0);
+$start = $pagination->prePagination();
+$query  = mysql_query("SELECT * FROM DM_noticia WHERE categoria=$categoria_id LIMIT $start, 10");
+
 ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
@@ -31,6 +44,9 @@ $rst_noticias=mysql_query("SELECT * FROM DM_noticia WHERE categoria=$categoria_i
         <title><?php echo $categoria_titulo; ?></title>
 
         <?php require_once("w-header-scripts.php") ?>
+
+        <!-- PAGINACION -->
+        <link rel="stylesheet" href="/libs/pagination/pagination.css" media="screen">
 
     </head>
     <body>
@@ -57,6 +73,8 @@ $rst_noticias=mysql_query("SELECT * FROM DM_noticia WHERE categoria=$categoria_i
                             </div>
 
                             <div class="contenido">
+
+                                <?php if($categoria_id==2 or $categoria_id==5){ ?>
                                 <ul>
 
                                     <?php while($fila_noticias=mysql_fetch_array($rst_noticias)){
@@ -71,6 +89,32 @@ $rst_noticias=mysql_query("SELECT * FROM DM_noticia WHERE categoria=$categoria_i
                                     <?php } ?>
                                     
                                 </ul>
+                                <?php }else{ ?>
+                                <?php while($row = mysql_fetch_assoc($query)){
+                                    $noticiacat_id=$row["id"];
+                                    $noticiacat_url=$row["url"];
+                                    $noticiacat_titulo=$row["titulo"];
+                                    $noticiacat_contenido=soloDescripcion($row["contenido"]);
+                                    $noticiacat_imagen=$row["imagen"];
+                                    $noticiacat_imagen_carpeta=$row["carpeta_imagen"];
+                                ?>
+                                
+                                    <article ="categoria_noticias">
+
+                                        <div class="imagen">
+                                            <img src="imagenes/upload/<?php echo $noticiacat_imagen_carpeta."".$noticiacat_imagen; ?>">
+                                        </div>
+
+                                        <div class="datos">
+                                            <h3><a href="<?php echo $url_categoria."/".$noticiacat_id."-".$noticiacat_url; ?>"><?php echo $noticiacat_titulo; ?></a></h3>
+                                            <p><?php echo $noticiacat_contenido; ?></p>
+                                        </div>
+
+
+                                    </article>
+                                
+                                <?php } $pagination->pagination(); } ?>
+
                             </div>
 
                         </div>
