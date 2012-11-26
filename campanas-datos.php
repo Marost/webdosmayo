757c8078ae9d45038db1915c95214aefba2e9b64
MARCO LOPEZ <mlopez@virtualsip.net>
2012-11-26 10:53:43 -0500

    opcion de noticias en campañpas

--- campanas-datos.php
+++ campanas-datos.php
@@ -29,44 +29,73 @@ $campania_dato=$_POST["tipo"];
     
 ?>
     
-    <ul>
-        <?php while($fila_cas_docs=mysql_fetch_array($rst_cas_docs)){
-            $docs_titulo=$fila_cas_docs["titulo"];
-            $docs_documento=$fila_cas_docs["documento"];
-            $docs_documento_tipo=$fila_cas_docs["documento_tipo"];
-        ?>
-            <li>
+    <table class="tabla_cas" width="700" border="0">
+        <tbody>
+            <tr>
+                <td class="dato_contenido" style="border:none;">
+                    <ul>
+                        <?php while($fila_cas_docs=mysql_fetch_array($rst_cas_docs)){
+                            $docs_titulo=$fila_cas_docs["titulo"];
+                            $docs_documento=$fila_cas_docs["documento"];
+                            $docs_documento_tipo=$fila_cas_docs["documento_tipo"];
+                        ?>
+                            <li>
 
-                <a title="<?php echo $docs_titulo; ?>" target="_blank" 
-                href="/documentos/<?php echo $cas_carpetas."".$docs_documento; ?>">
-                <span
-                    <?php if($docs_documento_tipo=="doc" or $docs_documento_tipo=="docx"){ ?>
-                        class="word"     
-                    <?php }elseif($docs_documento_tipo=="xls" or $docs_documento_tipo=="xlsx"){ ?>
-                        class="excel" 
-                    <?php }elseif($docs_documento_tipo=="ppt" or $docs_documento_tipo=="pptx" or $docs_documento_tipo=="pps" or $docs_documento_tipo=="ppsx"){ ?>
-                        class="pwpt" 
-                    <?php }elseif($docs_documento_tipo=="pdf"){ ?>
-                        class="pdf" 
-                    <?php }elseif($docs_documento_tipo=="gif"){ ?>
-                        class="gif" 
-                    <?php }elseif($docs_documento_tipo=="png"){ ?>
-                        class="png" 
-                    <?php }elseif($docs_documento_tipo=="bmp"){ ?>
-                        class="bmp" 
-                    <?php }elseif($docs_documento_tipo=="jpg" or $docs_documento_tipo=="jpeg" or $docs_documento_tipo=="JPG" or $docs_documento_tipo=="JPEG"){ ?>
-                        class="jpg" 
-                    <?php }elseif($docs_documento_tipo=="zip" or $docs_documento_tipo=="rar"){ ?>
-                        class="zip" 
-                    <?php } ?>
-                >
-                </span><p><?php echo $docs_titulo; ?></p></a>
+                                <a title="<?php echo $docs_titulo; ?>" target="_blank" 
+                                href="/documentos/<?php echo $cas_carpetas."".$docs_documento; ?>">
+                                <span
+                                    <?php if($docs_documento_tipo=="doc" or $docs_documento_tipo=="docx"){ ?>
+                                        class="word"     
+                                    <?php }elseif($docs_documento_tipo=="xls" or $docs_documento_tipo=="xlsx"){ ?>
+                                        class="excel" 
+                                    <?php }elseif($docs_documento_tipo=="ppt" or $docs_documento_tipo=="pptx" or $docs_documento_tipo=="pps" or $docs_documento_tipo=="ppsx"){ ?>
+                                        class="pwpt" 
+                                    <?php }elseif($docs_documento_tipo=="pdf"){ ?>
+                                        class="pdf" 
+                                    <?php }elseif($docs_documento_tipo=="gif"){ ?>
+                                        class="gif" 
+                                    <?php }elseif($docs_documento_tipo=="png"){ ?>
+                                        class="png" 
+                                    <?php }elseif($docs_documento_tipo=="bmp"){ ?>
+                                        class="bmp" 
+                                    <?php }elseif($docs_documento_tipo=="jpg" or $docs_documento_tipo=="jpeg" or $docs_documento_tipo=="JPG" or $docs_documento_tipo=="JPEG"){ ?>
+                                        class="jpg" 
+                                    <?php }elseif($docs_documento_tipo=="zip" or $docs_documento_tipo=="rar"){ ?>
+                                        class="zip" 
+                                    <?php } ?>
+                                >
+                                </span><p><?php echo $docs_titulo; ?></p></a>
 
-            </li>
-        <?php } ?>
-    </ul>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 
 <?php }elseif ($campania_dato=="nota-prensa"){ ?>
+
+    <!-- ALTO DE DIV -->
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script type="text/javascript">
+    var jald = jQuery.noConflict();
+    function equalHeight(group) {
+       tallest = 0;
+       group.each(function() {
+          thisHeight = jald(this).height();
+          if(thisHeight > tallest) {
+             tallest = thisHeight;
+          }
+       });
+       group.height(tallest);
+    }
+
+    jald(document).ready(function() {
+       equalHeight(jald(".categoria_noticias"));
+    });
+
+    </script>
 
     <?php
 
@@ -87,7 +116,7 @@ $campania_dato=$_POST["tipo"];
 
         <?php if($noticias_imagen<>""){ ?>
         <div class="datos">
-            <h3><a href="<?php echo $noticias_categoria["url"]."/".$noticias_id."-".$noticias_url; ?>"><?php echo $noticias_titulo; ?></a></h3>
+            <h3><a target="_blank" href="campanias/nota/<?php echo $noticias_id."/".$noticias_url; ?>"><?php echo $noticias_titulo; ?></a></h3>
             <div class="imagen">
                 <img src="imagenes/upload/<?php echo $noticias_imagen_carpeta."thumb200/".$noticias_imagen; ?>" width="300" height="200" alt="<?php echo $noticias_titulo; ?>">
             </div>
@@ -97,7 +126,7 @@ $campania_dato=$_POST["tipo"];
         <?php }else{ ?>
         
         <div class="datos an100">
-            <h3><a href="<?php echo $noticias_categoria["url"]."/".$noticias_id."-".$noticias_url; ?>"><?php echo $noticias_titulo; ?></a></h3>
+            <h3><a target="_blank" href="campanias/nota/<?php echo $noticias_id."/".$noticias_url; ?>"><?php echo $noticias_titulo; ?></a></h3>
             <p><?php echo $noticias_contenido; ?></p>
             <p class="fecha"><?php echo nombreFecha($noticias_fecha[0],$noticias_fecha[1],$noticias_fecha[2]); ?></p>
         </div>
---
+++ campanas-nota.php
@@ -0,0 +1,152 @@
+<?php
+/*CONEXION Y FUNCIONES*/
+require_once("panel@hndm/conexion/conexion.php");
+require_once("panel@hndm/conexion/funciones.php");
+require_once("panel@hndm/conexion/funcion-paginacion.php");
+
+/*SCRIPT*/
+$script_slide_noticia=true;
+
+/*VARIABLES DE URL*/
+$url_categoria=$_REQUEST["cat"];
+$url_noticia_id=$_REQUEST["id"];
+$url_noticia_url=$_REQUEST["url"];
+$url_web=$web."".$url_categoria."/".$url_noticia_id."-".$url_noticia_url;
+
+/*NOTICIA*/
+$rst_noticia=mysql_query("SELECT * FROM DM_campanias_noticia WHERE id=$url_noticia_id;", $conexion);
+$fila_noticia=mysql_fetch_array($rst_noticia);
+
+/*VARIABLES DE NOTICIA*/
+$noticia_titulo=$fila_noticia["titulo"];
+$noticia_contenido=$fila_noticia["contenido"];
+$noticia_contenido_comp=explode(soloDescripcion($noticia_contenido), $noticia_contenido);
+$noticia_imagen=$fila_noticia["imagen"];
+$noticia_imagen_carpeta=$fila_noticia["carpeta_imagen"];
+$noticia_categoria=$fila_noticia["categoria"];
+
+/*NOTICIA - SLIDE*/
+$rst_noticia_slide=mysql_query("SELECT * FROM DM_campanias_noticia_slide WHERE noticia=$url_noticia_id ORDER BY orden ASC;", $conexion);
+$num_noticia_slide=mysql_num_rows($rst_noticia_slide);
+
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title><?php echo $noticia_titulo; ?></title>
+        
+        <!-- OPEN GRAPH -->
+        <meta property="og:title" content="<?php echo $noticia_titulo; ?>" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="<?php echo $url_web; ?>" />
+        <meta property="og:image" content="<?php echo $web.""."/imagenes/upload/".$noticia_imagen_carpeta."".$noticia_imagen; ?>" />
+        <meta property="og:site_name" content="<?php echo $web_nombre; ?>" />
+        <meta property="og:description" content="<?php echo soloDescripcion($noticia_contenido); ?>"/>
+        <meta property="fb:admins" content="1376286793" />
+
+        <?php require_once("w-header-scripts.php") ?>
+
+        <!-- SLIDE NOTICIA -->
+        <link href="/libs/allinone_banner/allinone_thumbnailsBanner.css" rel="stylesheet" type="text/css">
+        <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
+        <script src="/libs/allinone_banner/jquery.ui.touch-punch.min.js"></script>
+        <script src="/libs/allinone_banner/jquery.mousewheel.min.js"></script>
+        <script src="/libs/allinone_banner/allinone_thumbnailsBanner.js"></script>
+        <script src="/libs/allinone_banner/reflection.js" ></script>
+        <!--[if IE]><script src="/libs/allinone_banner/excanvas.compiled.js" ></script><![endif]-->
+        <script>
+        var jNotSld=jQuery.noConflict();
+        jNotSld(document).ready(function(){
+            jNotSld('.imagen_slide div').allinone_thumbnailsBanner({
+                skin: 'cool',
+                numberOfThumbsPerScreen:4,
+                width: 620,
+                height: 360,
+                thumbsWrapperMarginTop:0
+            });
+        });
+        </script>
+
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <div id="interior">
+
+            <div class="header-container">
+                
+                <?php require_once("w-header.php") ?>
+
+            </div>
+
+            <div class="main-container">
+                <div class="main wrapper clearfix">
+
+                    <section id="news">
+
+                        <div class="nw-nota borde-bottom">
+
+                            <div class="titulo">
+                                <h2><?php echo $noticia_titulo; ?></h2>
+                            </div>
+
+                            <div class="contenido">
+                                <p><?php echo soloDescripcion($noticia_contenido); ?></p>
+                            </div>
+
+                            <?php if($noticia_imagen<>""){ ?>
+                            <div class="imagen">
+                                <img alt="<?php echo $noticia_titulo; ?>" title=" " src="/imagenes/upload/<?php echo $noticia_imagen_carpeta."".$noticia_imagen; ?>">
+                            </div>
+                            <?php } ?>
+
+                            <div class="contenido">
+                                <?php echo $noticia_contenido_comp[1]; ?>
+                            </div>
+
+                            <?php if($num_noticia_slide>0){ ?>
+                            <div class="imagen_slide">
+
+                                <div style="display:none;">
+                                
+                                    <ul class="allinone_bannerWithPlaylist_list">   
+                                        <?php while ($fila_noticia_slide=mysql_fetch_array($rst_noticia_slide)){
+                                            /*VARIABLES DE SLIDE*/
+                                            $noticia_slide_id=$fila_noticia_slide["id"];
+                                            $noticia_slide_imagen=$fila_noticia_slide["imagen"];
+                                            $noticia_slide_imagen_carpeta=$fila_noticia_slide["carpeta"];
+                                        ?>
+                                            <li data-bottom-thumb="/imagenes/upload/<?php echo $noticia_slide_imagen_carpeta."thumb/".$noticia_slide_imagen; ?>">
+                                          <img src="/imagenes/upload/<?php echo $noticia_slide_imagen_carpeta."".$noticia_slide_imagen; ?>" alt="" /></li>
+                                        <?php } ?>
+                                    </ul>
+                                </div>
+
+                            </div>
+                            <?php } ?>
+
+                        </div>
+
+                    </section>
+
+                    <?php require_once("w-sidebar.php") ?>
+
+                </div> <!-- #main -->
+            </div> <!-- #main-container -->
+
+            <?php require_once("w-footer.php") ?>
+
+        </div>
+
+<?php require_once("w-popup.php") ?>
+
+    </body>
+</html>
