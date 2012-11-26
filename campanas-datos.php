765fc54fefd3147dd3515e8986fbd913f0723223
MARCO LOPEZ <mlopez@virtualsip.net>
2012-11-26 10:21:32 -0500

    agregando tabla a lista de rebote de medios en campaña

--- campanas-datos.php
+++ campanas-datos.php
@@ -113,41 +113,49 @@ $campania_dato=$_POST["tipo"];
     $rst_cas_docs=mysql_query("SELECT * FROM DM_campania_rebote WHERE cas=$campania_id ORDER BY orden ASC;", $conexion);
 ?>
     
-    <ul>
-        <?php while($fila_cas_docs=mysql_fetch_array($rst_cas_docs)){
-            $docs_titulo=$fila_cas_docs["titulo"];
-            $docs_documento=$fila_cas_docs["documento"];
-            $docs_documento_tipo=$fila_cas_docs["documento_tipo"];
-        ?>
-            <li>
+    <table class="tabla_cas" width="700" border="0">
+        <tbody>
+            <tr>
+                <td class="dato_contenido">
+                    <ul>
+                        <?php while($fila_cas_docs=mysql_fetch_array($rst_cas_docs)){
+                            $docs_titulo=$fila_cas_docs["titulo"];
+                            $docs_documento=$fila_cas_docs["documento"];
+                            $docs_documento_tipo=$fila_cas_docs["documento_tipo"];
+                        ?>
+                            <li>
 
-                <a title="<?php echo $docs_titulo; ?>" target="_blank" 
-                href="/documentos/<?php echo $cas_carpetas."".$docs_documento; ?>">
-                <span
-                    <?php if($docs_documento_tipo=="doc" or $docs_documento_tipo=="docx"){ ?>
-                        class="word"     
-                    <?php }elseif($docs_documento_tipo=="xls" or $docs_documento_tipo=="xlsx"){ ?>
-                        class="excel" 
-                    <?php }elseif($docs_documento_tipo=="ppt" or $docs_documento_tipo=="pptx" or $docs_documento_tipo=="pps" or $docs_documento_tipo=="ppsx"){ ?>
-                        class="pwpt" 
-                    <?php }elseif($docs_documento_tipo=="pdf"){ ?>
-                        class="pdf" 
-                    <?php }elseif($docs_documento_tipo=="gif"){ ?>
-                        class="gif" 
-                    <?php }elseif($docs_documento_tipo=="png"){ ?>
-                        class="png" 
-                    <?php }elseif($docs_documento_tipo=="bmp"){ ?>
-                        class="bmp" 
-                    <?php }elseif($docs_documento_tipo=="jpg" or $docs_documento_tipo=="jpeg" or $docs_documento_tipo=="JPG" or $docs_documento_tipo=="JPEG"){ ?>
-                        class="jpg" 
-                    <?php }elseif($docs_documento_tipo=="zip" or $docs_documento_tipo=="rar"){ ?>
-                        class="zip" 
-                    <?php } ?>
-                >
-                </span><p><?php echo $docs_titulo; ?></p></a>
+                                <a title="<?php echo $docs_titulo; ?>" target="_blank" 
+                                href="/documentos/<?php echo $cas_carpetas."".$docs_documento; ?>">
+                                <span
+                                    <?php if($docs_documento_tipo=="doc" or $docs_documento_tipo=="docx"){ ?>
+                                        class="word"     
+                                    <?php }elseif($docs_documento_tipo=="xls" or $docs_documento_tipo=="xlsx"){ ?>
+                                        class="excel" 
+                                    <?php }elseif($docs_documento_tipo=="ppt" or $docs_documento_tipo=="pptx" or $docs_documento_tipo=="pps" or $docs_documento_tipo=="ppsx"){ ?>
+                                        class="pwpt" 
+                                    <?php }elseif($docs_documento_tipo=="pdf"){ ?>
+                                        class="pdf" 
+                                    <?php }elseif($docs_documento_tipo=="gif"){ ?>
+                                        class="gif" 
+                                    <?php }elseif($docs_documento_tipo=="png"){ ?>
+                                        class="png" 
+                                    <?php }elseif($docs_documento_tipo=="bmp"){ ?>
+                                        class="bmp" 
+                                    <?php }elseif($docs_documento_tipo=="jpg" or $docs_documento_tipo=="jpeg" or $docs_documento_tipo=="JPG" or $docs_documento_tipo=="JPEG"){ ?>
+                                        class="jpg" 
+                                    <?php }elseif($docs_documento_tipo=="zip" or $docs_documento_tipo=="rar"){ ?>
+                                        class="zip" 
+                                    <?php } ?>
+                                >
+                                </span><p><?php echo $docs_titulo; ?></p></a>
 
-            </li>
-        <?php } ?>
-    </ul>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 
 <?php } ?>
