993f71914c95eaf732b1bf193709c0b39951b9b1
MARCO LOPEZ <mlopez@virtualsip.net>
2012-12-16 23:40:28 -0500

    opcion de lista de empresas en footer

--- w-footer.php
+++ w-footer.php
@@ -1,91 +1,29 @@
+<?php
+/*LISTA DE EMPRESAS*/
+$rst_list_empresas=mysql_query("SELECT * FROM DM_lista_empresas ORDER BY id ASC;", $conexion);
+?>
 <div class="footer-container">
     <footer class="wrapper">
         
         <section id="enlaces-interes">
 
             <marquee>
-            
-                <div class="items">
-                    <a href="http://www.peru.gob.pe">
-                        <img alt="" src="imagenes/empresas/gobierno.jpg" width="132" height="37">
-                    </a>
-                </div>
                 
-                <div class="items">
-                    <a href="http://minsa.gob.pe">
-                    <img alt="" src="imagenes/empresas/ministerio-salud.jpg" width="80" height="37">
-                    </a>
-                </div>
-                
-                <div class="items">
-                    <a href="http://www.mac.pe/">
-                    <img alt="" src="imagenes/empresas/bann_mac.jpg" width="108" height="37">
-                    </a>
-                </div>
-                
-                <div class="items">
-                    <a href="http://www.sis.gob.pe/Portal/index.html">
-                    <img alt="" src="imagenes/empresas/sis.jpg" height="37"></a>
-                </div>
-                
-                <div class="items">
-                    <a href="http://www.minsa.gob.pe/samu/">
-                    <img alt="" src="imagenes/empresas/samu.png" width="211" height="37"></a>
-                </div>                
-                
-                     <a href="http://www.peru.gob.pe">
-                        <img alt="" src="imagenes/empresas/gobierno.jpg" width="132" height="37">
-                    </a>
-                </div>
-                
-                <div class="items">
-                    <a href="http://minsa.gob.pe">
-                    <img alt="" src="imagenes/empresas/ministerio-salud.jpg" width="80" height="37">
-                    </a>
-                </div>
-                
-                <div class="items">
-                    <a href="http://www.mac.pe/">
-                    <img alt="" src="imagenes/empresas/bann_mac.jpg" width="108" height="37">
-                    </a>
-                </div>
-                
-                <div class="items">
-                    <a href="http://www.sis.gob.pe/Portal/index.html">
-                    <img alt="" src="imagenes/empresas/sis.jpg" height="37"></a>
-                </div>
-                
-                <div class="items">
-                    <a href="http://www.minsa.gob.pe/samu/">
-                    <img alt="" src="imagenes/empresas/samu.png" width="211" height="37"></a>
-                </div>   
+                <?php while($fila_list_empresas=mysql_fetch_array($rst_list_empresas)){
+                        $lsempresas_titulo=$fila_list_empresas["titulo"];
+                        $lsempresas_imagen=$fila_list_empresas["imagen"];
+                        $lsempresas_imagen_carpeta=$fila_list_empresas["carpeta_imagen"];
+                        if ($fila_list_empresas["enlace"]==""){ $lsempresas_enlace="javascript:;";
+                        }else{ $lsempresas_enlace=$fila_list_empresas["enlace"]; }
+                ?>
 
-                     <a href="http://www.peru.gob.pe">
-                        <img alt="" src="imagenes/empresas/gobierno.jpg" width="132" height="37">
+                <div class="items">
+                    <a href="<?php echo $lsempresas_enlace; ?>" title="<?php echo $lsempresas_titulo; ?>">
+                        <img alt="" src="/imagenes/upload/<?php echo $lsempresas_imagen_carpeta."/".$lsempresas_imagen; ?>" alt="<?php echo $lsempresas_titulo; ?>">
                     </a>
                 </div>
-                
-                <div class="items">
-                    <a href="http://minsa.gob.pe">
-                    <img alt="" src="imagenes/empresas/ministerio-salud.jpg" width="80" height="37">
-                    </a>
-                </div>
-                
-                <div class="items">
-                    <a href="http://www.mac.pe/">
-                    <img alt="" src="imagenes/empresas/bann_mac.jpg" width="108" height="37">
-                    </a>
-                </div>
-                
-                <div class="items">
-                    <a href="http://www.sis.gob.pe/Portal/index.html">
-                    <img alt="" src="imagenes/empresas/sis.jpg" height="37"></a>
-                </div>
-                
-                <div class="items">
-                    <a href="http://www.minsa.gob.pe/samu/">
-                    <img alt="" src="imagenes/empresas/samu.png" width="211" height="37"></a>
-                </div>   
+
+                <?php } ?>
                 
             </marquee>
         </section>
